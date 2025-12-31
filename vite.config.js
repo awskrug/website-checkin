da5bb5ab75abeff17f232640b8d2e2abc50e2bd2
hyunje <hyunje@example.com>
2025-12-31 19:15:47 +0900

    init: upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/website-checkin/',
+  server: {
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+})
+
