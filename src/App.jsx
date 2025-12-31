9b93cdbbdbd584eaa3c2bc0c0ee53e0513784554
hyunje <hyunje@example.com>
2025-12-31 19:31:13 +0900

    fix: event code가 없어도 앱에 접속되도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 소모임 + 이벤트 코드 경로 */}
-        <Route path="/:groupCode/:eventCode" element={<CheckInPage />} />
-        
         {/* 결과 페이지 */}
         <Route path="/:groupCode/:eventCode/result" element={<ResultPage />} />
-        
-        {/* 기본 경로 - sls/test로 리다이렉트 */}
-        <Route path="/" element={<Navigate to="/sls/test" replace />} />
-        
+
+        {/* 소모임 + 이벤트 코드 경로 */}
+        <Route path="/:groupCode/:eventCode" element={<CheckInPage />} />
+
+        {/* 소모임만 있는 경로 (eventCode 기본값 사용) */}
+        <Route path="/:groupCode" element={<CheckInPage />} />
+
+        {/* 기본 경로 - sls로 리다이렉트 */}
+        <Route path="/" element={<Navigate to="/sls" replace />} />
+
         {/* 404 - 기본 페이지로 */}
-        <Route path="*" element={<Navigate to="/sls/test" replace />} />
+        <Route path="*" element={<Navigate to="/sls" replace />} />
       </Routes>
     </BrowserRouter>
   );
